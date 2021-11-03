65e2bbd1cbfe69826f951c5bce5273e909cdb30f
Stefan <stefanketa99@gmail.com>
2021-11-03 22:16:23 +0200

    Fix the layouts

--- app/src/main/java/com/example/licenta/util/Util.kt
+++ app/src/main/java/com/example/licenta/util/Util.kt
@@ -9,6 +9,18 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 
 object Util {
+    const val MINIMUM_HEIGHT_CM = 54
+    const val MAXIMUM_HEIGHT_CM = 250
+    const val MINIMUM_WEIGHT_KG = 25
+
+    fun convertFeetInchesToCm(feet : Int, inches : Int = 0) : Double{
+        return (feet.toFloat()*30.48f + inches.toFloat()*2.54f).toDouble()
+    }
+
+    fun convertLbsToKg(lbs:Int):Double{
+        return (lbs.toDouble()/2.2046)
+    }
+
     fun hideKeyboard(activity: Activity){
         val inputManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         if(inputManager.isAcceptingText)
