d41627336a07b4af8d4e6f7aec4570735799f077
Stefan <stefanketa99@gmail.com>
2021-11-06 23:27:02 +0200

    Implement the profile fragment tab functionality

--- app/src/main/java/com/example/licenta/fragment/main/ProfileFragment.kt
+++ app/src/main/java/com/example/licenta/fragment/main/ProfileFragment.kt
@@ -2,11 +2,21 @@ package com.example.licenta.fragment.main
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.licenta.R
+import com.example.licenta.fragment.main.profile.ConnectionsFragment
+import com.example.licenta.fragment.main.profile.GoalsFragment
+import com.example.licenta.fragment.main.profile.PostsFragment
+import com.example.licenta.fragment.main.profile.RecordsFragment
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,17 +28,30 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ProfileFragment : Fragment(), TabLayout.OnTabSelectedListener{
+    private lateinit var fragmentFrameLayout : FrameLayout
+    private lateinit var profilePhoto : ImageView
+    private lateinit var settings : ImageView
+    private lateinit var infoTab: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    }
+
+    private fun initComponents(view:View){
+        fragmentFrameLayout = view.findViewById(R.id.fragment_profile_items_fragment)
+        infoTab = view.findViewById(R.id.fragment_profile_tab_layout)
+        infoTab.addOnTabSelectedListener(this)
+        profilePhoto = view.findViewById(R.id.fragment_profile_photo_profile_iv)
+        settings = view.findViewById(R.id.fragment_profile_button_settings_btn)
+    }
+
+
+
+
+    private fun switchFragment(fragment: Fragment){
+        Toast.makeText(context,"Welcome to ${fragment.javaClass}",Toast.LENGTH_SHORT)
+            .show()
     }
 
     override fun onCreateView(
@@ -36,7 +59,9 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        initComponents(view)
+        return view
     }
 
     companion object {
@@ -57,5 +82,22 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        when(tab!!.position) {
+            0 -> switchFragment(PostsFragment())
+            1 -> switchFragment(GoalsFragment())
+            2 -> switchFragment(RecordsFragment())
+            3 -> switchFragment(ConnectionsFragment())
+        }
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+        //
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+        //
     }
 }
