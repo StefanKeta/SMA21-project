c8c0f521a1febf005654ec59c536f3308ff92a08
Stefan <stefanketa99@gmail.com>
2021-11-06 23:26:18 +0200

    Implement the FAB design

--- app/src/main/java/com/example/licenta/activity/MainActivity.kt
+++ app/src/main/java/com/example/licenta/activity/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.licenta.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import com.example.licenta.R
@@ -11,12 +14,30 @@ import com.example.licenta.fragment.main.HomeFragment
 import com.example.licenta.fragment.main.ProfileFragment
 import com.example.licenta.fragment.main.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener{
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener, View.OnClickListener{
 
     private lateinit var navigationBar : BottomNavigationView
     private lateinit var fragmentLayout : FrameLayout
+    private lateinit var addFab : FloatingActionButton
+    private lateinit var addExerciseFab : FloatingActionButton
+    private lateinit var addFoodFab : FloatingActionButton
+    private val rotateOpenAnim : Animation by lazy {
+        AnimationUtils.loadAnimation(this,R.anim.fab_rotate_open)
+    }
+    private val rotateCloseAnim : Animation by lazy {
+        AnimationUtils.loadAnimation(this,R.anim.fab_rotate_close)
+    }
+    private val fromBottomAnim : Animation by lazy {
+        AnimationUtils.loadAnimation(this,R.anim.fab_from_bottom)
+    }
+    private val toBottomAnim : Animation by lazy {
+        AnimationUtils.loadAnimation(this,R.anim.fab_to_bottom)
+    }
+
+    private var clicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +51,12 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         navigationBar.selectedItemId = R.id.menu_main_bottom_home
         switchFragments(HomeFragment())
         navigationBar.setOnItemSelectedListener(this)
+        addFab = findViewById(R.id.activity_main_fab_add)
+        addFab.setOnClickListener(this)
+        addFoodFab = findViewById(R.id.activity_main_fab_food)
+        addFoodFab.setOnClickListener(this)
+        addExerciseFab = findViewById(R.id.activity_main_fab_exercise)
+        addExerciseFab.setOnClickListener(this)
     }
 
 
@@ -39,10 +66,16 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
             R.id.menu_main_bottom_search -> switchFragments(SearchFragment())
             R.id.menu_main_bottom_diary ->  switchFragments(DiaryFragment())
             R.id.menu_main_bottom_profile -> switchFragments(ProfileFragment())
-            
+
 
         }
         return true
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.activity_main_fab_add -> onAddFabClicked()
+        }
     }
 
     private fun switchFragments(fragment:Fragment){
@@ -52,5 +85,33 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         fragmentTransaction.commit()
     }
 
+    private fun onAddFabClicked(){
+        setButtonsVisibility(clicked)
+        setButtonsAnimation(clicked)
+        clicked = !clicked
+    }
+
+    private fun setButtonsVisibility(clicked:Boolean){
+        if (!clicked){
+            addExerciseFab.visibility = View.VISIBLE
+            addFoodFab.visibility = View.VISIBLE
+        }else{
+            addExerciseFab.visibility = View.INVISIBLE
+            addFoodFab.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setButtonsAnimation(clicked: Boolean){
+        if (!clicked){
+            addExerciseFab.startAnimation(fromBottomAnim)
+            addFoodFab.startAnimation(fromBottomAnim)
+            addFab.startAnimation(rotateOpenAnim)
+        }else{
+            addExerciseFab.startAnimation(toBottomAnim)
+            addFoodFab.startAnimation(toBottomAnim)
+            addFab.startAnimation(rotateCloseAnim)
+        }
+
+    }
 
 }
