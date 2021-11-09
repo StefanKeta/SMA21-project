0825b1dbbfa56debd42f81cb08c61fbf84315e30
Stefan <stefanketa99@gmail.com>
2021-11-09 22:44:46 +0200

    Implement the basic diary design and food tab basic design

--- app/src/main/java/com/example/licenta/fragment/main/DiaryFragment.kt
+++ app/src/main/java/com/example/licenta/fragment/main/DiaryFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.example.licenta.R
+import com.example.licenta.fragment.main.diary.ExerciseFragment
+import com.example.licenta.fragment.main.diary.FoodFragment
+import com.google.android.material.tabs.TabLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,15 +22,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DiaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var tabLayout : TabLayout
+    private lateinit var fragmentLayout : FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            param1 = it.getString(com.example.licenta.fragment.main.ARG_PARAM1)
+            param2 = it.getString(com.example.licenta.fragment.main.ARG_PARAM2)
         }
     }
 
@@ -34,8 +39,39 @@ class DiaryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+        val view = inflater.inflate(R.layout.fragment_diary, container, false)
+        initComponents(view)
+        return view
+    }
+
+    private fun initComponents(view : View){
+        tabLayout = view.findViewById(R.id.fragment_diary_tab_layout)
+        fragmentLayout = view.findViewById(R.id.fragment_diary_fragment_frame_layout)
+        switchFragment(FoodFragment())
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab!!.position == 0) switchFragment(FoodFragment())
+                else switchFragment(ExerciseFragment())
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                if (tab!!.position == 0) switchFragment(FoodFragment())
+                else switchFragment(ExerciseFragment())
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                if (tab!!.position == 0) switchFragment(FoodFragment())
+                else switchFragment(ExerciseFragment())
+            }
+
+        })
+    }
+
+    private fun switchFragment(fragment : Fragment){
+        val fragmentManager = parentFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_diary_fragment_frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 
     companion object {
@@ -45,15 +81,15 @@ class DiaryFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DiaryFragment.
+         * @return A new instance of fragment FoodFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DiaryFragment().apply {
+            FoodFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(com.example.licenta.fragment.main.ARG_PARAM1, param1)
+                    putString(com.example.licenta.fragment.main.ARG_PARAM2, param2)
                 }
             }
     }
